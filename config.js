ebf7a44886ca96564f91eccc2f6019f4e58749b4
Bijan <71988155+stockholmsbif@users.noreply.github.com>
2025-07-28 14:42:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // config.js
 
-//OLD export const COURSE_API_URL = 'https://script.google.com/macros/s/AKfycbxyQcYafsWscDmtkq_u6NNugKjfFKW9eoU1bYAE0nRKz10PxQrsvMxLUGzPmz_zdwFSZQ/exec';
 export const COURSE_API_URL = 'https://stockholmsbif.github.io/kursfilter-cdn/courses.json';
 
 
