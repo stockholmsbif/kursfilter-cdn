e52ca946651c7ab9a96ee00ed91fecdb4df76073
Bijan <71988155+stockholmsbif@users.noreply.github.com>
2025-07-29 02:14:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 //Detta syns endast i Github
 
 export const COURSE_API_URL = 'https://stockholmsbif.github.io/kursfilter-cdn/courses.json';
+export const CONTACT_API_URL = 'https://script.google.com/macros/s/AKfycbwzDbeGyk4xCrUfXEgcFfLJHsY3zzXftRVxv7BczVptnYQ6H2YLtsaXnPFNccaOdPVTSA/exec';
+export const CONTACT_API_TOKEN = 'hemligKod123';
 
 // Exempel för framtiden:
 // export const CONTACT_ENDPOINT = '/wp-json/kursfilter/v1/contact';
