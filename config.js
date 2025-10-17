7942ac1bd9f23149dc70a18eb306581be1a22e55
Bijan <71988155+stockholmsbif@users.noreply.github.com>
2025-10-17 22:54:11 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-//Detta syns endast i Github
-
-export const COURSE_API_URL = 'https://stockholmsbif.github.io/kursfilter-cdn/courses.json';
-export const CONTACT_API_URL = 'https://script.google.com/macros/s/AKfycbyOr8JPI8HLXuW-ukjLFCXowV7GLRnWmZ_u7pPvZmnytnS2FG6zN7k2TgxMGWFhPtuGLg/exec';
-export const CONTACT_API_TOKEN = 'dinHemligaKod';
-
-// Exempel för framtiden:
-// export const CONTACT_ENDPOINT = '/wp-json/kursfilter/v1/contact';
