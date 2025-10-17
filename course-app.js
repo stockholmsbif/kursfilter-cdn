0787e9af8f901f0c7916127cdd74bf331defc8a7
Bijan <71988155+stockholmsbif@users.noreply.github.com>
2025-10-17 22:54:20 +0200

    Delete course-app.js

--- course-app.js
+++
@@ -1,8 +0,0 @@
-import { COURSE_API_URL } from './config.js';
-import { CourseBrowserApp } from './components/CourseBrowserApp.js';
-
-export function renderCourseBrowser({ React, ReactDOM }) {
-  const e = React.createElement;
-  const root = ReactDOM.createRoot(document.getElementById('course-browser'));
-  root.render(e(CourseBrowserApp));
-}
